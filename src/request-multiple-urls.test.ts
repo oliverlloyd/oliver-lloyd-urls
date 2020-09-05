2615d8048c87d4ebc524c2a8aced130db2dce34b
Oliver Lloyd <email@oliverlloyd.com>
2020-09-05 10:18:30 +0100

    Added some requirements to start with

---
+++ src/request-multiple-urls.test.ts
@@ -0,0 +1,7 @@
+describe("When requesting multiple urls", () => {
+  it.todo("accepts an array of urls and returns an array of responses");
+  it.todo("handles when an empty array is passed");
+  it.todo("will also accept a single url as a string");
+  it.todo("will populate the responses array with any errors");
+  it.todo("includes the orginal url in the error response");
+});
